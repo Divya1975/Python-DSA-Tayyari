51af8909866931b22cbf463a603b4a7fc3772a23
DIVYA JOSHI <divyajosji04.ujn@gmail.com>
2025-07-06 18:20:25 +0530

    Create operator_practice.py

---
+++ 03_Operators/03_Operators/03_Operators/operator_practice.py
@@ -0,0 +1,19 @@
+# PQ1
+x = 15
+y = 4
+print(x % y)  # Output?
+
+# PQ2
+a = 10
+a *= 2
+print(a)
+
+# PQ3
+print(5 > 3 and 2 < 1)  # False
+
+# PQ4
+num = 8
+print(num // 3)  # Floor division
+
+# PQ5
+print("Divya" in "My name is Divya")  # True
